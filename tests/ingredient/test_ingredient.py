eddf7ac302632dc20dc2427fc512a4fe1a51b771
Elienay Alves <elienay8486@gmail.com>
2023-05-18 22:39:01 -0300

    feat(test_ingredient.py): finish requirement 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,3 +4,10 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 # Req 1
 def test_ingredient():
     pass
+    ingredient = Ingredient("leite")
+
+    assert repr(ingredient) == "Ingredient('leite')"
+    assert hash(ingredient) == hash("leite")
+    assert ingredient.name == "leite"
+    assert ingredient == Ingredient("leite")
+    assert not ingredient.restrictions
