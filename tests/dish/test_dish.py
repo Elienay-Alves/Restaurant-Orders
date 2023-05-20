3a952992f54e0420ba709a30a81ae692bfeab87a
Elienay Alves <elienay8486@gmail.com>
2023-05-20 02:12:39 -0300

    feat(test_dish.py): finish requirement 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,25 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish("Pizza", 28.9)
+
+    with pytest.raises(ValueError):
+        Dish("Pizza", 0)
+
+    assert dish == Dish("Pizza", 28.9)
+    assert dish.name == "Pizza"
+    assert dish.price == 28.9
+    assert hash(dish) == hash("Dish('Pizza', R$28.90)")
+    assert repr(dish) == "Dish('Pizza', R$28.90)"
+
+    dish.add_ingredient_dependency(Ingredient("Churrasco"), 1)
+
+    assert dish.recipe == {Ingredient("Churrasco"): 1}
+    assert {ingredient.name for ingredient in dish.get_ingredients()} == {
+        "Churrasco"
+    }
+    assert not dish.get_restrictions()
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,7 +3,6 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
     ingredient = Ingredient("leite")
 
     assert repr(ingredient) == "Ingredient('leite')"
